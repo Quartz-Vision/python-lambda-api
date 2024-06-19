8f2e74286db9abce87c0ea0b3228cba010fa4772
Lia Va <mistickusya.2012@gmail.com>
2024-06-19 08:02:04 -0600

    v 1.0.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(ROOT / "requirements.txt") as fr:
 
 setuptools.setup(
     name="lambda-api",
-    version="0.2.2",
+    version="1.0.0",
     author="liava",
     author_email="liava@tuta.io",
     description="Minimal Web API for lambdas",
