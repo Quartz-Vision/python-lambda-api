bd22181e30570263107fee69465e0554ad211846
Lia Va <mistickusya.2012@gmail.com>
2024-08-01 01:14:16 -0600

    update error handling

--- examples/call_endpoints.py
+++ examples/call_endpoints.py
@@ -68,4 +68,4 @@ async def main():
     )
 
 
-asyncio.get_event_loop().run_until_complete(main())
+asyncio.run(main())
--- lambda_api/adapters.py
+++ lambda_api/adapters.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from typing import Any, Callable
 
 from pydantic import BaseModel, ValidationError
@@ -7,12 +8,14 @@ from lambda_api.core import InvokeTemplate, LambdaAPI, Response
 from lambda_api.error import APIError
 from lambda_api.schema import Method
 
+logger = logging.getLogger(__name__)
+
 
 class AWSAdapter:
     def __init__(self, app: LambdaAPI):
         self.app = app
 
-    def request(self, event: dict[str, Any]) -> dict[str, Any]:
+    def parse_request(self, event: dict[str, Any]) -> dict[str, Any]:
         path = "/" + event.get("pathParameters", {}).get("proxy", "").strip("/")
         method = event["httpMethod"]
 
@@ -38,7 +41,7 @@ class AWSAdapter:
             "provider_data": event,
         }
 
-    def response(self, response: Response):
+    def make_response(self, response: Response):
         return {
             "statusCode": response.status,
             "body": json.dumps(response.body),
@@ -50,7 +53,7 @@ class AWSAdapter:
 
     async def lambda_handler(self, event: dict[str, Any], context: Any = None):
         try:
-            request = self.request(event)
+            request = self.parse_request(event)
 
             endpoint = self.app.route_table.get(request["path"])
             method = request["method"]
@@ -74,14 +77,33 @@ class AWSAdapter:
         except APIError as e:
             response = Response(status=e.status, body={"error": str(e)})
         except ValidationError as e:
-            response = Response(status=400, body={"error": e.errors()})
+            response = Response(status=400, body={"error": e.json()})
         except Exception as e:
             response = Response(status=500, body={"error": str(e)})
 
-        return self.response(response)
+        return self.make_response(response)
 
     async def run_handler(self, func: Callable, request: dict[str, Any]) -> Response:
         template: InvokeTemplate = func.__invoke_template__  # type: ignore
+
+        # this ValidationError is raised when the request data is invalid
+        # we can return it to the client
+        try:
+            args = self.collect_args(request, template)
+        except ValidationError as e:
+            return Response(status=400, body={"error": e.json()})
+
+        result = await func(**args)
+
+        # this ValidationError is raised when the response data is invalid
+        # we can log it and return a generic error to the client to avoid leaking
+        try:
+            return self.dump_response(result, template)
+        except ValidationError as e:
+            logger.error(f"Response data is invalid.\nREQUEST:\n{request}\nERROR:\n{e}")
+            return Response(status=500, body={"error": "Internal Server Error"})
+
+    def collect_args(self, request: dict[str, Any], template: InvokeTemplate) -> dict:
         args = {}
 
         if template.request:
@@ -91,7 +113,9 @@ class AWSAdapter:
         if template.body:
             args["body"] = template.body(**request["body"])
 
-        result = await func(**args)
+        return args
+
+    def dump_response(self, result: Any, template: InvokeTemplate) -> Response:
         if template.response:
             model = template.response
             status = template.status
@@ -101,7 +125,7 @@ class AWSAdapter:
             elif template.user_root_response:
                 response = Response(status, model(result).model_dump(mode="json"))
             else:
-                return Response(status, model(**result).model_dump(mode="json"))
+                response = Response(status, model(**result).model_dump(mode="json"))
         else:
             response = Response(status=template.status, body=None)
 
